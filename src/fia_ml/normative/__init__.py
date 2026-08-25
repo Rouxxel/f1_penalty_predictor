c7b2e1b74f5fa591b00630a5400ea686945df016
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-08-25 10:34:15 +0200

    Create __init__.py

---
+++ src/fia_ml/normative/__init__.py
@@ -0,0 +1,5 @@
+"""Prescriptive normative stewarding rule engine."""
+
+from fia_ml.normative.rules_loader import LoadedRules, load_rules, rules_file_hash
+
+__all__ = ["LoadedRules", "load_rules", "rules_file_hash"]
